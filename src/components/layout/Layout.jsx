e1dde23928595ba7595cff43a9de9ad4d2c74115
Yaraslau Fedarovich <jar.vladimirovich@gmail.com>
2024-08-07 10:36:13 +0300

    router

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,87 +1,28 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React from "react"
+import { Link, Route, Switch } from "react-router-dom";
+import { Game } from "./components/Game";
 import styles from "../styles/styles.module.scss"
 
 const webApp = window.Telegram.WebApp;
 
 export function Layout() {
-  const [count, setCount] = useState(0)
-  const [level, setLevel] = useState("a1")
-  const [reversed, setReversed] = useState(false)
-  const [showCount, setShowCount] = useState(false)
-  const [select, setSelect] = useState("r")
-  const [platform, setPlatform] = useState("hello")
-
-  const onSendData = useCallback(() => {
-    const data = select + (reversed ? "-r" : "") + " " + level + " " + (select === "g" || select === "p" ? count : "")
-    webApp.sendData(data)
-  }, [count, reversed, select, level])
-
-  useEffect(() => {
-    webApp.ready()
-    webApp.expand()
-    setPlatform(webApp.platform)
-  }, [])
-
-  useEffect(() => {
-    if ((count === 0 || count > 50) && (select === "g" || select === "p")) {
-      webApp.MainButton.disable()
-    } else {
-      webApp.MainButton.enable()
-    }
-  }, [count, select])
-
-  useEffect(() => {
-    webApp.MainButton.setParams({
-      text: "Start game",
-    });
-    webApp.MainButton.show()
-  }, [])
-
-  useEffect(() => {
-    webApp.onEvent('mainButtonClicked', onSendData)
-    return () => {
-      webApp.offEvent('mainButtonClicked', onSendData)
-    }
-  }, [onSendData])
-
-  const handleSelectChange = (e) => {
-    setShowCount(e === "g" || e === "p")
-    setSelect(e)
-  }
-
-  const handleCountChange = (e) => {
-    setCount(e)
-    if (e > 0 && e <= 50) {
-      webApp.MainButton.enable()
-    } else {
-      webApp.MainButton.disable()
-    }
-  }
-
-  return (<div className={styles.form}>
-    <h3>Game types</h3>
-    <select value={select}  onChange={(e) => handleSelectChange(e.target.value)} className={styles.select}>
-      <option value={'r'}>Random</option>
-      <option value={'g'}>Random with count</option>
-      <option value={'g-e'}>Endless game</option>
-      <option value={'p'}>Practice</option>
-    </select>
-    <h3>English level</h3>
-    <select value={level}  onChange={(e) => setLevel(e.target.value)} className={styles.select}>
-      <option value={'a1'}>A1</option>
-      <option value={'a2'}>A2</option>
-      <option value={'b1'}>B1</option>
-      <option value={'b2'}>B2</option>
-      <option value={'c1'}>C1</option>
-    </select>
-    {select !== "p" && <div>
-      <h3>Reversed</h3>
-      <input value={reversed} onChange={(e) => setReversed(!reversed)} type={"checkbox"}/>
-    </div>}
-    {showCount && <React.Fragment>
-      <h3>Count</h3>
-      <input value={count} type={"number"} onChange={(e) => handleCountChange(e.target.value)} className={styles.input}/>
-    </React.Fragment>}
-    <div>{platform}</div>
-  </div>);
+   return (<React.Fragment>
+    <Switch>
+      <Route exact path={"/"}>
+      <div className={styles.startGameContainer}>
+        <div className={styles.startPage}>
+          <h3>
+            START PAGE
+          </h3>
+          <button className={styles.startGameButton}>
+            <Link to={"/game"}>START GAME</Link>
+          </button>
+        </div>
+      </div>
+      </Route>
+      <Route path={"/game"}>
+       <Game webApp={webApp}/>
+      </Route>
+    </Switch>
+  </React.Fragment>);
 }
