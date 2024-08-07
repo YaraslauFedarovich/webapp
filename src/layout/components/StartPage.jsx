1036ef5722d25b6026dfc660952af3676c4759ad
Yaraslau Fedarovich <jar.vladimirovich@gmail.com>
2024-08-07 18:15:00 +0300

    add animations

--- src/layout/components/StartPage.jsx
+++ src/layout/components/StartPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "../styles.module.scss";
 import { Link } from "react-router-dom";
-import { SlideLeftAnimation } from "../../animation/SlideLeftAnimation";
+import { ShowAnimation } from "../../animation/ShowAnimation";
 
 export function StartPage({ actions }) {
 
   return (<div className={styles.startGameContainer}>
     <div className={styles.startPage}>
-     <SlideLeftAnimation>
+     <ShowAnimation>
        <div className={styles.labelFlex}><h3> START PAGE</h3></div>
-     </SlideLeftAnimation>
+     </ShowAnimation>
       <button className={styles.stepButton}>
         <Link to={"/level"}>Next</Link>
       </button>
